46ada0a6f08a7eb31faa8a44b05659d6328739b1
noi01 <n.balska@gmail.com>
2018-09-16 02:03:40 +0500

    sensor reading

--- environment_01.py
+++ environment_01.py
@@ -6,6 +6,8 @@ import random
 import RPi.GPIO as GPIO
 import time
 
+#SETUP OUTPUT
+
 GPIO.setmode(GPIO.BOARD)
 
 GPIO.setup(11, GPIO.OUT)
@@ -18,11 +20,30 @@ def Forward():
 def Snooze():
 	GPIO.output(11, 0)
 	GPIO.output(13, 0)
+	
+#SETUP INPUT
 
-#sensor_input = 1 #integer mock input
+#sensor_input = 1 #integer mock input variation 1
+#sensor_input = [0, 1] #mock input variation 2
+	
+pin_to_circuit = 7 #define the pin that goes to the circuit
 
-sensor_input = [0, 1]
+def rc_time (pin_to_circuit):
+    count = 0
+  
+    #Output on the pin for 
+    GPIO.setup(pin_to_circuit, GPIO.OUT)
+    GPIO.output(pin_to_circuit, GPIO.LOW)
+    time.sleep(0.1)
 
+    #Change the pin back to input
+    GPIO.setup(pin_to_circuit, GPIO.IN)
+  
+    #Count until the pin goes high
+    while (GPIO.input(pin_to_circuit) == GPIO.LOW):
+        count += 1
+
+    return count
 
 class Env(Environment):
     """ Environment for RL walking robot """       
@@ -40,7 +61,10 @@ class Env(Environment):
             :rtype: by default, this is assumed to be a numpy array of doubles
         """
         
-        sensor_value = random.choice(sensor_input)
+        #sensor_value = sensor_input #mock input variation 1
+        #sensor_value = random.choice(sensor_input)#mock input variation 2
+        sensor_value = rc_time (pin_to_circuit)
+        
         print sensor_value 
         
         return [float(sensor_value),]
